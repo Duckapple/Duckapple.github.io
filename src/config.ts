9687a5bbda68372a853c4ae935d1f02227deb9e4
Simon Green Kristensen <git@simon-green.dev>
2023-04-23 22:49:14 +0200

    We GREEN now

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,7 @@
 
 export const SITE_TITLE = "Simon Green";
 export const SITE_DESCRIPTION = "Hey and welcome to my website!";
+export const GRADIENT =
+  "bg-gradient-to-br from-lime-700 dark:from-lime-600 to-lime-500 dark:to-lime-300";
+export const GREEN_STYLE =
+  "text-transparent font-bold bg-clip-text " + GRADIENT;
