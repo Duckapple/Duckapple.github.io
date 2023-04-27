c0af3076742469e8cd70da858c9a4589bca3f3b1
Simon Green Kristensen <git@simon-green.dev>
2023-04-28 00:14:31 +0200

    Nice blog stylings and a custom handler for images to create captions

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,70 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 import solidJs from "@astrojs/solid-js";
 
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+
+import { normalizeUri } from "micromark-util-sanitize-uri";
+
+// @ts-check
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://simon-green.dev",
-  integrations: [mdx(), sitemap(), tailwind(), solidJs()]
+  integrations: [mdx(), sitemap(), tailwind(), solidJs()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      [
+        rehypeKatex,
+        {
+          // Katex plugin options
+          macros: {},
+        },
+      ],
+    ],
+    remarkRehype: {
+      handlers: {
+        image: (state, node) => {
+          const properties = { src: normalizeUri(node.url) };
+
+          if (node.alt !== null && node.alt !== undefined) {
+            properties.alt = node.alt;
+          }
+
+          /** @type {Element} */
+          let elem = {
+            type: "element",
+            tagName: "img",
+            properties,
+            children: [],
+          };
+          state.patch(node, elem);
+
+          if (node.title != null) {
+            elem = {
+              type: "element",
+              tagName: "figure",
+              properties: {},
+              children: [
+                elem,
+                {
+                  type: "element",
+                  tagName: "figcaption",
+                  properties: {},
+                  children: [{ type: "text", value: node.title }],
+                },
+              ],
+            };
+          }
+
+          return state.applyData(node, elem);
+        },
+      },
+    },
+    shikiConfig: {
+      theme: "rose-pine",
+      // theme: "rose-pine-moon",
+    },
+  },
 });
